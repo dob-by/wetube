4473de92a17a07035681511c827bc44a9b6362ab
yeonhwalim <yeonhwalim03@gmail.com>
2023-06-09 18:53:17 +0900

    Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,8 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
+  return res.render("watch");
 };
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+export const edit = (req, res) => res.render("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
