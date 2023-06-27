0ad3abb3564582a141ea8fed30e5d12eacedf1d8
yeonhwalim <yeonhwalim03@gmail.com>
2023-06-27 22:40:02 +0900

    Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,12 +2,30 @@ export const trending = (req, res) => {
   const videos = [
     {
       title: "Hello",
+      titls: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
       title: "Video #2",
+      titls: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
       title: "Whatsup",
+      titls: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
   ];
   res.render("home", { pageTitle: "Home", videos });
