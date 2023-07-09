6e129eacf3f627b5e608c03395f75552f2b781c8
yeonhwalim <yeonhwalim03@gmail.com>
2023-07-09 15:13:12 +0900

    Array Database Part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,12 +4,11 @@ import {
   edit,
   upload,
   deleteVideo,
-  trending,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", trending);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
