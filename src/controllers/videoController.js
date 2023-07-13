21e0129a71f3624ff874a6c416fe56ef14531e7c
yeonhwalim <yeonhwalim03@gmail.com>
2023-07-13 23:02:54 +0900

    Edit Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,13 +33,23 @@ export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("watch", {
-    pageTitle: `Watching ${video.title}`,
+    pageTitle: `Watching: ${video.title}`,
     video: video,
   });
 };
-export const edit = (req, res) => res.render("Edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  res.send("Delete Vidio");
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", {
+    pageTitle: `Editing: ${video.title}`,
+    video,
+  });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
 };
