429121c01ea0da3afaf07c3f96ee827b9b70db01
yeonhwalim <yeonhwalim03@gmail.com>
2023-10-12 11:51:37 +0900

    Create Account

--- src/db.js
+++ src/db.js
@@ -1,7 +1,9 @@
 // #6.8
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+  useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 
