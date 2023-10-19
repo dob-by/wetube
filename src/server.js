6b3221cc6bba3e8ad16f9ee65629a66111e52502
yeonhwalim <yeonhwalim03@gmail.com>
2023-10-19 23:35:04 +0900

    Logged In User

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middleware";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,6 +13,17 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+// session은 라우터 앞. session middleware가 사이트로 들어오는 모두를 기억
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware); // localsMiddleware가 session middleware 다음에 와야 함
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
