81819d4e4a56ff2ebf613c51ce6e32531dbee1c1
yeonhwalim <yeonhwalim03@gmail.com>
2023-11-17 15:34:43 +0900

    Github Login

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -17,9 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 // session은 라우터 앞. session middleware가 사이트로 들어오는 모두를 기억
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
